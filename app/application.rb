9e5925e6d004ca35cbad7c1fe7aae6ee2873dfad
github username <github email address>
2019-10-05 12:57:14 +0100

    Done

--- app/application.rb
+++ app/application.rb
@@ -9,22 +9,25 @@ class Application
 
     if req.path.match(/items/)
       @@items.each do |item|
-        resp.write "#{item}\n"
-      end
+        resp.write "#{item}\n" end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      if @@cart[0]
-        @@cart.each do | item |
-          resp.write "#{item}\n"
-        end
-      else
+      if @@cart.empty?
         resp.write "Your cart is empty"
-      end
-    elsif req.path.match(/add/)
-      item = req.params["item"]
-      resp.write handle_add(item)
+      else
+        @@cart.each {|item| 
+        resp.write "#{item}\n"}
+        end
+      elsif req.path.match(/add/)
+        add_items = req.params["item"]
+          if @@items.include?(add_items)
+            @@cart << add_items
+            resp.write "added #{add_items}"
+          else 
+            resp.write "We don't have that item!"
+          end 
     else
       resp.write "Path Not Found"
     end
@@ -39,14 +42,4 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
-
-  def handle_add(item)
-    if @@items.include?(item)
-      @@cart << item
-      return "added #{item}"
-    else
-      return "We don't have that item"
-    end
-  end
-
 end
